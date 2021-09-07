228a989e7698af7d3eea4cbbd7ba1c317492dd9c
Rich Dunne <richdunne@yahoo.com>
2021-09-07 06:12:02 -0600

    Initial commit

---
+++ CourseManager/ViewModels/AddInstructorViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CourseManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CourseManager.ViewModels
+{
+    public class AddInstructorViewModel : BaseViewModel
+    {
+        private string _instructorName;
+        public string InstructorName { get => _instructorName; set => SetProperty(ref _instructorName, value); }
+
+        private string _instructorPhoneNumber;
+        public string InstructorPhoneNumber { get => _instructorPhoneNumber; set => SetProperty(ref _instructorPhoneNumber, value); }
+
+        private string _instructorEmail;
+        public string InstructorEmail { get => _instructorEmail; set => SetProperty(ref _instructorEmail, value); }
+
+        public Command NavigateAddAssessmentsCommand { get; }
+
+        public AddInstructorViewModel()
+        {
+            Title = "Add Instructor";
+            NavigateAddAssessmentsCommand = new Command(NavigateAddAssessments);
+
+        }
+
+        private async void NavigateAddAssessments()
+        {
+            if(string.IsNullOrWhiteSpace(_instructorName))
+            {
+                return;
+            }
+
+            //var route = $"{nameof(AddInstructorPage)}";
+            //await Shell.Current.GoToAsync(route);
+        }
+    }
+}
