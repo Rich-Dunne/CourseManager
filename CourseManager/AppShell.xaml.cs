0ec6f5886291ad2e54092ed9384a63de45afc9ef
Rich Dunne <richdunne@yahoo.com>
2021-09-07 04:41:15 -0600

    Added routing for AddTermPage and ModifyTermPage

--- CourseManager/AppShell.xaml.cs
+++ CourseManager/AppShell.xaml.cs
@@ -13,11 +13,13 @@ namespace CourseManager
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(AddTermPage), typeof(AddTermPage));
+            Routing.RegisterRoute(nameof(ModifyTermPage), typeof(ModifyTermPage));
         }
 
-        private async void OnMenuItemClicked(object sender, EventArgs e)
-        {
-            await Shell.Current.GoToAsync("//LoginPage");
-        }
+        //private async void OnMenuItemClicked(object sender, EventArgs e)
+        //{
+        //    await Shell.Current.GoToAsync("//LoginPage");
+        //}
     }
 }
