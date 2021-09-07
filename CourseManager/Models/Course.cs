59135b1be261244c454a3fc8115d7ad257a2cb34
Rich Dunne <richdunne@yahoo.com>
2021-09-07 06:12:32 -0600

    Removed unnecessary property

--- CourseManager/Models/Course.cs
+++ CourseManager/Models/Course.cs
@@ -17,7 +17,6 @@ namespace CourseManager.Models
         public Instructor Instructor { get; set; }
         public Status Status { get; set; }
         public string Notes { get; set; }
-        public string CourseInformation { get; set; }
         public bool EnableNotifications { get; set; }
         public int AssociatedTermId { get; set; }
 
