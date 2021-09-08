52f15728f0f694dc6ff5150cc21b8d9931c6cf0c
Rich Dunne <richdunne@yahoo.com>
2021-09-08 07:08:07 -0600

    Commented out unused code

--- CourseManager/Services/TermService.cs
+++ CourseManager/Services/TermService.cs
@@ -28,18 +28,18 @@ namespace CourseManager.Services
             await database.CreateTableAsync<Term>();
         }
 
-        public static async Task ListTables()
-        {
-            Debug.WriteLine($"Below is a list of tables which exist in the database:");
-            foreach (var table in database.TableMappings)
-            {
-                var tableInfo = await database.GetTableInfoAsync(table.TableName);
-                if (tableInfo.Count > 0)
-                {
-                    Debug.WriteLine($"{table.TableName} ({tableInfo.Count} columns)");
-                }
-            }
-        }
+        //public static async Task ListTables()
+        //{
+        //    Debug.WriteLine($"Below is a list of tables which exist in the database:");
+        //    foreach (var table in database.TableMappings)
+        //    {
+        //        var tableInfo = await database.GetTableInfoAsync(table.TableName);
+        //        if (tableInfo.Count > 0)
+        //        {
+        //            Debug.WriteLine($"{table.TableName} ({tableInfo.Count} columns)");
+        //        }
+        //    }
+        //}
 
         public static async Task ImportTerms()
         {
@@ -71,31 +71,32 @@ namespace CourseManager.Services
                     TermGroups.Add(new TermGroup(result.Id, result.TermName, result.StartDate, result.EndDate, new List<Course>() { newCourse }));
                     Debug.WriteLine($"Added new term group to collection.");
                 }
+                // Else get courses with matching Term ID and add them to the existingTermGroup courses
             }
         }
 
 
-        public static void GetTables()
-        {
-            var table = database.Table<Term>();
-            if(table == null)
-            {
-                Debug.WriteLine($"The Terms table does not exist.");
-                return;
-            }
+        //public static void GetTables()
+        //{
+        //    var table = database.Table<Term>();
+        //    if(table == null)
+        //    {
+        //        Debug.WriteLine($"The Terms table does not exist.");
+        //        return;
+        //    }
 
-            var query = table.Where(x => x.Id >= 0);
-            if(query.CountAsync().Result == 0)
-            {
-                Debug.WriteLine($"There are no terms in the table.");
-            }
+        //    var query = table.Where(x => x.Id >= 0);
+        //    if(query.CountAsync().Result == 0)
+        //    {
+        //        Debug.WriteLine($"There are no terms in the table.");
+        //    }
 
-            query.ToListAsync().ContinueWith((t) =>
-            {
-                foreach (var term in t.Result)
-                    Debug.WriteLine($"Term: {term.TermName}");
-            });
-        }
+        //    query.ToListAsync().ContinueWith((t) =>
+        //    {
+        //        foreach (var term in t.Result)
+        //            Debug.WriteLine($"Term: {term.TermName}");
+        //    });
+        //}
 
         public static async Task AddTerm(string termName, DateTime startDate, DateTime endDate)
         {
@@ -154,20 +155,32 @@ namespace CourseManager.Services
             Debug.WriteLine($"({term.Id}) \"{term.TermName}\" removed.");
         }
 
-        public static async Task<IEnumerable<Term>> GetTerms()
+        public static async Task AddCourseToTerm(Term term, Course newCourse)
         {
-            await Init();
+            var matchingGroup = TermGroups.FirstOrDefault(x => x.Id == term.Id);
+            if (matchingGroup == null)
+            {
+                Debug.WriteLine($"Matching group not found.");
+            }
 
-            var term = await database.Table<Term>().ToListAsync();
-            return term;
+            matchingGroup.Courses.Add(newCourse);
+            await ImportTerms();
         }
 
-        public static async Task DropTable()
-        {
-            await Init();
+        //public static async Task<IEnumerable<Term>> GetTerms()
+        //{
+        //    await Init();
 
-            await database.DropTableAsync<Term>().ContinueWith((results) => Debug.WriteLine($"Table deleted."));
-        }
+        //    var term = await database.Table<Term>().ToListAsync();
+        //    return term;
+        //}
+
+        //public static async Task DropTable()
+        //{
+        //    await Init();
+
+        //    await database.DropTableAsync<Term>().ContinueWith((results) => Debug.WriteLine($"Table deleted."));
+        //}
 
         public static async Task ClearTable()
         {
@@ -186,19 +199,19 @@ namespace CourseManager.Services
             await ImportTerms();
         }
 
-        public static void GetTableRows()
-        {
-            var query = database.QueryAsync<Term>("SELECT * from Term");
+        //public static void GetTableRows()
+        //{
+        //    var query = database.QueryAsync<Term>("SELECT * from Term");
 
-            if(query.Result.Count == 0)
-            {
-                Debug.WriteLine($"There are no terms.");
-            }
+        //    if(query.Result.Count == 0)
+        //    {
+        //        Debug.WriteLine($"There are no terms.");
+        //    }
 
-            foreach(var t in query.Result)
-            {
-                Debug.WriteLine($"Result: {t.TermName}");
-            }
-        }
+        //    foreach(var t in query.Result)
+        //    {
+        //        Debug.WriteLine($"Result: {t.TermName}");
+        //    }
+        //}
     }
 }
