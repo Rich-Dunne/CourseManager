fd3574e27b32e751fc8b547b82900c352223faae
Rich Dunne <richdunne@yahoo.com>
2021-09-11 09:06:53 -0600

    Form values are passed from the previous form and to the next form.

--- CourseManager/ViewModels/AddInstructorViewModel.cs
+++ CourseManager/ViewModels/AddInstructorViewModel.cs
@@ -2,16 +2,31 @@
 using CourseManager.Views;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace CourseManager.ViewModels
 {
+    [QueryProperty(nameof(CourseValues), nameof(CourseValues))]
     public class AddInstructorViewModel : BaseViewModel
     {
-        private string _instructorName;
-        public string InstructorName { get => _instructorName; set => SetProperty(ref _instructorName, value); }
+        private string _courseValues;
+        public string CourseValues
+        {
+            get => _courseValues;
+            set
+            {
+                SetProperty(ref _courseValues, value);
+            }
+        }
+
+        private string _instructorFirstName;
+        public string InstructorFirstName { get => _instructorFirstName; set => SetProperty(ref _instructorFirstName, value); }
+
+        private string _instructorLastName;
+        public string InstructorLastName { get => _instructorLastName; set => SetProperty(ref _instructorLastName, value); }
 
         private string _instructorPhoneNumber;
         public string InstructorPhoneNumber { get => _instructorPhoneNumber; set => SetProperty(ref _instructorPhoneNumber, value); }
@@ -27,17 +42,17 @@ namespace CourseManager.ViewModels
             Title = "Add Instructor";
             NavigateAddAssessmentsCommand = new Command(NavigateAddAssessments);
             NavigateBackCommand = new Command(NavigateBack);
-
         }
 
         private async void NavigateAddAssessments()
         {
-            if(string.IsNullOrWhiteSpace(_instructorName))
+            if(string.IsNullOrWhiteSpace(_instructorFirstName))
             {
                 return;
             }
 
-            var route = $"{nameof(AddAssessmentsPage)}";
+            var instructorValues = $"{InstructorFirstName},{InstructorLastName},{InstructorPhoneNumber},{InstructorEmail}";
+            var route = $"{nameof(AddAssessmentsPage)}?CourseValues={CourseValues}&InstructorValues={instructorValues}";
             await Shell.Current.GoToAsync(route);
         }
 
