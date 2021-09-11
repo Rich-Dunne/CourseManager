222a1e129dbb6cffeaa2be58983301ec40e2a2f3
Rich Dunne <richdunne@yahoo.com>
2021-09-11 09:03:08 -0600

    Removed DataStore class

--- CourseManager/App.xaml.cs
+++ CourseManager/App.xaml.cs
@@ -12,7 +12,6 @@ namespace CourseManager
 
         public App()
         {
-            DataStore.InitializeData();
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
