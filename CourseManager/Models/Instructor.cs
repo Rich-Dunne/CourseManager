41d417f52804eacd5ed3468c5c49700da76f766c
Rich Dunne <richdunne@yahoo.com>
2021-09-12 10:18:31 -0600

    Added SQLite attributes

--- CourseManager/Models/Instructor.cs
+++ CourseManager/Models/Instructor.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CourseManager.Models
 {
+    [Table("Instructors")]
     public class Instructor
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
-        public int AssociatedCourseId { get; set; }
 
         public Instructor() { }
     }
