cfb2515b1d63755d1d0b3f134714cfeb5d766225
Rich Dunne <richdunne@yahoo.com>
2021-09-12 10:18:03 -0600

    Added SQLite attributes

--- CourseManager/Models/Assessment.cs
+++ CourseManager/Models/Assessment.cs
@@ -1,17 +1,20 @@
 ﻿using CourseManager.Enums;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CourseManager.Models
 {
+    [Table("Assessments")]
     public class Assessment
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public string Name { get; set; }
         public AssessmentType AssessmentType { get; set; }
         public DateTime DueDate { get; set; }
         public bool EnableNotifications { get; set; }
-        public int AssociatedCourseId { get; set; }
 
         public Assessment() { }
     }
